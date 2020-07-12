c5da9d421ae58c6e506b03a5cee2af3358091bb4
Elizabeth Garza <garza.elizabeth9@gmail.com>
2020-07-12 17:24:07 -0400

    added the option to mk_clsfrs in batches

--- src/train_nltk_clsfrs.py
+++ src/train_nltk_clsfrs.py
@@ -28,13 +28,16 @@ if __name__ =="__main__":
 
     os.chdir('..')
     os.chdir('data')
-    counts_of_mellizas = pandas.read_csv("top_200_mellizas.csv", sep=",")
-    new_counts = counts_of_mellizas.head(int(args.no_of_clsfrs))
-    max1_max2_tokens = new_counts["MAX1_TOKEN"] + "\t"+ new_counts["MAX2_TOKEN"] 
+    nrows = int(args.no_of_clsfrs)
+    # changes the rows that you want to iterate over by adding the argument, e.g. skiprows=(23, 23 +nrows, 1)
+    mellizas = pandas.read_csv("top_200_mellizas.csv", sep=",", nrows=nrows)
+    max1_max2_tokens = mellizas["MAX1_TOKEN"] + "\t"+ mellizas["MAX2_TOKEN"] 
     for row in tqdm(max1_max2_tokens): 
         tokenized_row = row.split()
         os.chdir('pickles')
         saved_clsfr = open(f"{unidecode.unidecode(tokenized_row[0])}.pickle", "wb") 
         pickle.dump(mk_clsfr(args.corpus_path, tokenized_row[0], tokenized_row[-1]), saved_clsfr)
         saved_clsfr.close()
+
+        
 
