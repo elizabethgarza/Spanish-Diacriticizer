61d9ed8b791238e7efcdbdf6d981008ec9ad3632
Elizabeth Garza <garza.elizabeth9@gmail.com>
2020-07-14 16:12:31 -0400

    changed some var. inconsistencies, etc.

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,25 +2,19 @@ import os
 from nltk import word_tokenize
 from tqdm import tqdm
 
-"""
-TODOs:  
- 1.  merge mk_clsfr_dict with this, but use the top_600_mellizas 
- 2.  possible change the title of mk_clsfr_dict to mk_clsfrs
- 3.  run on a baby file to test, then do everything.
- """
 
 class Preprocess: 
     
-    def __init__(self, corpus, token1, token2): 
+    def __init__(self, corpus, melliza1, melliza2): 
         
         self.corpus = corpus 
-        self.token1 = token1 
-        self.token2 = token2 
+        self.melliza1 = melliza1 
+        self.melliza2 = melliza2 
         
-    def find_sentences_for_token1(self): 
+    def find_sentences_for_melliza1(self): 
 
         os.chdir('/Users/mariagarza/Desktop/nltk-Spanish-diacriticizer/data')
-        with open(self.corpus, "r") as source: 
+        with open(self.corpus, 'r') as source: 
             sentence_set = set()
             lines = []
             i=0
@@ -28,66 +22,65 @@ class Preprocess:
                 tokenized_line = line.split()
                 for token in tokenized_line: 
                     lowercased_token = token.casefold()
-                    if self.token1 == lowercased_token:
-                        lines.append(" ".join(tokenized_line))
+                    if self.melliza1 == lowercased_token:
+                        lines.append(' '.join(tokenized_line))
             for line in lines: 
                 sentence_set.add(line)
 
             return list(sentence_set)
         
-    def find_sentences_for_token2(self): 
+    def find_sentences_for_melliza2(self): 
         
-        with open(self.corpus, "r") as source: 
+        with open(self.corpus, 'r') as source: 
             sentence_set = set()
             lines = []
-            i=0
             for line in tqdm(source):
                 tokenized_line = line.split()
                 for token in tokenized_line: 
                     lowercased_token = token.casefold()
-                    if self.token2 == lowercased_token:
-                        lines.append(" ".join(tokenized_line))
+                    if self.melliza2 == lowercased_token:
+                        lines.append(' '.join(tokenized_line))
             for line in lines: 
                 sentence_set.add(line)
 
             return list(sentence_set)
         
     @staticmethod
-    def get_sentence_info_for_feature_extraction(sentence, token_of_interest): 
+    def get_sentence_info_for_feature_extraction(sentence, melliza_of_interest): 
     
         tokenized_sentence = word_tokenize(sentence)
-        token_of_interest_index = -1
-        for item in tokenized_sentence:
-            casefolded_token = item.casefold()
-            token_of_interest_index +=1
-            if casefolded_token == token_of_interest: 
+        melliza_of_interest_index = -1
+        for token in tokenized_sentence:
+            casefolded_token = token.casefold()
+            melliza_of_interest_index +=1
+            if casefolded_token == melliza_of_interest: 
                 break
 
-        return tokenized_sentence, token_of_interest_index
+        return tokenized_sentence, melliza_of_interest_index
 
     
-    def mk_token1_token2_list(self): 
+    def mk_melliza1_melliza2_list(self): 
         
-        token1_sentence_info_list = []
-        for sentence in tqdm(Preprocess.find_sentences_for_token1(self)): 
-            token1_sentence_info_list.append((Preprocess.get_sentence_info_for_feature_extraction(sentence, self.token1), self.token1))
+        melliza1_sentence_info_list = []
+        for sentence in tqdm(Preprocess.find_sentences_for_melliza1(self)): 
+            melliza1_sentence_info_list.append((Preprocess.get_sentence_info_for_feature_extraction(sentence, self.melliza1), self.melliza1))
         
-        token2_sentence_info_list = []
-        for sentence in tqdm(Preprocess.find_sentences_for_token2(self)): 
-            token2_sentence_info_list.append((Preprocess.get_sentence_info_for_feature_extraction(sentence, self.token2), self.token2))
+        melliza2_sentence_info_list = []
+        for sentence in tqdm(Preprocess.find_sentences_for_melliza2(self)): 
+            melliza2_sentence_info_list.append((Preprocess.get_sentence_info_for_feature_extraction(sentence, self.melliza2), self.melliza2))
             
-        token1_token2_info_list = token1_sentence_info_list + token2_sentence_info_list
+        melliza1_melliza2_info_list = melliza1_sentence_info_list + melliza2_sentence_info_list
         
-        return token1_token2_info_list 
+        return melliza1_melliza2_info_list 
 
     def mk_train_and_test_set(self): 
         
-        token1_token2_sentences_and_labels = Preprocess.mk_token1_token2_list(self)
+        melliza1_melliza2_sentences_and_labels = Preprocess.mk_melliza1_melliza2_list(self)
         #random.seed(5228554)
-        #random.shuffle(token1_token2_sentences_and_labels)
-        train_length = round(.8 * len(token1_token2_sentences_and_labels))    
-        train = token1_token2_sentences_and_labels[:train_length]
-        test = token1_token2_sentences_and_labels[train_length:]
+        #random.shuffle(melliza1_melliza2_sentences_and_labels)
+        train_length = round(.8 * len(melliza1_melliza2_sentences_and_labels))    
+        train = melliza1_melliza2_sentences_and_labels[:train_length]
+        test = melliza1_melliza2_sentences_and_labels[train_length:]
         
         return train, test
 
--- src/train_nltk_clsfrs.py
+++ src/train_nltk_clsfrs.py
@@ -10,34 +10,84 @@ import preprocess
 import clsfr_prep
 
 
-def mk_clsfr(corpus_path, token1, token2): 
+def mk_clsfr(corpus_path, melliza1, melliza2): 
 
-    train_prep, test_prep = preprocess.Preprocess(args.corpus_path, token1, token2).mk_train_and_test_set()
+    train_prep, test_prep = preprocess.Preprocess(corpus_path, melliza1, melliza2).mk_train_and_test_set()
     train = clsfr_prep.Clsfr_Prep(train_prep, test_prep).mk_nltk_train_set()
     test = clsfr_prep.Clsfr_Prep(train_prep, test_prep).mk_nltk_test_set()
     nltk_clsfr = nltk.classify.naivebayes.NaiveBayesClassifier.train(train)
-    print(f"{unidecode.unidecode(token1)} accuracy:  {nltk.classify.util.accuracy(nltk_clsfr, test):.4f}")
+    print(f"{unidecode.unidecode(melliza1)} accuracy:  {nltk.classify.util.accuracy(nltk_clsfr, test):.4f}")
     
     return nltk_clsfr
 
-if __name__ =="__main__": 
-    parser = argparse.ArgumentParser(description="Trains nltk naive bayes classifiers and loads them into pickle files.")
-    parser.add_argument("corpus_path", help="str: path to corpus file")
-    parser.add_argument("no_of_clsfrs", help="int: number of classifiers that you want to train; recommended amount: 200")
+if __name__ =='__main__': 
+    parser = argparse.ArgumentParser(description='Trains nltk naive bayes classifiers and loads them into pickle files')
+    parser.add_argument('corpus_path', help='str: path to corpus file')
+    parser.add_argument('-no', help='int: number of classifiers that are to be trained')
+    parser.add_argument('-m1_m2', help='str: melliza pair to be classified with dash in between; e.g. `él_el` ')
     args = parser.parse_args()
 
-    os.chdir('..')
-    os.chdir('data')
-    nrows = int(args.no_of_clsfrs)
-    # changes the rows that you want to iterate over by adding the argument, e.g. skiprows=(1, 23, 1)
-    mellizas = pandas.read_csv("top_200_mellizas.csv", sep=",", skiprows = [i for i in range(1, 23)], nrows=nrows)
-    max1_max2_tokens = mellizas["MAX1_TOKEN"] + "\t"+ mellizas["MAX2_TOKEN"] 
-    for row in tqdm(max1_max2_tokens): 
-        tokenized_row = row.split()
-        os.chdir('pickles')
-        saved_clsfr = open(f"{unidecode.unidecode(tokenized_row[0])}.pickle", "wb") 
-        pickle.dump(mk_clsfr(args.corpus_path, tokenized_row[0], tokenized_row[-1]), saved_clsfr)
+    # option to train on a specific number of clsfrs, rather than the default no., 200.
+    if args.no: 
+        os.chdir('..')
+        os.chdir('data')
+        # if you are making classifiers in batches, add argument `skiprows = [i for i in range(1,no_of_rows_to_skip)]` 
+        mellizas = pandas.read_csv('top_200_mellizas.csv', sep=',', nrows=int(args.no))
+        max1_max2_tokens = mellizas['MAX1_TOKEN'] + '\t'+ mellizas['MAX2_TOKEN'] 
+        for row in tqdm(max1_max2_tokens): 
+            tokenized_row = row.split()
+            os.chdir('pickles')
+            melliza1 = tokenized_row[0]
+            melliza2 = tokenized_row[-1]
+            saved_clsfr = open(f'{unidecode.unidecode(melliza1)}.pickle', 'wb')
+            pickle.dump(mk_clsfr(args.corpus_path, melliza1, melliza2), saved_clsfr)
+            saved_clsfr.close()
+    # option to train a single clsfr
+    elif args.m1_m2:
+        os.chdir('..')
+        os.chdir('data/pickles')
+        melliza1 = args.m1_m2.split('_')[0]
+        melliza2 = args.m1_m2.split('_')[-1]
+        clsfr = mk_clsfr(args.corpus_path, melliza1, melliza2)
+        saved_clsfr = open(clsfr, saved_clsfr)
         saved_clsfr.close()
+    # default algorithm to classify top 200 mellizas.
+    else:
+        os.chdir('..')
+        os.chdir('data')
+        # if you are making classifiers in batches, add argument `skiprows = [i for i in range(1,23)]` if you want to, 
+        # e.g. skip the first 23 rows.
+        mellizas = pandas.read_csv('top_200_mellizas.csv', sep=',')
+        max1_max2_tokens = mellizas['MAX1_TOKEN'] + "\t"+ mellizas['MAX2_TOKEN'] 
+        for row in tqdm(max1_max2_tokens): 
+            tokenized_row = row.split()
+            os.chdir('pickles')
+            melliza1 = tokenized_row[0]
+            melliza2 = tokenized_row[-1]
+            saved_clsfr = open(f'{unidecode.unidecode(melliza1)}.pickle', 'wb') 
+            pickle.dump(mk_clsfr(args.corpus_path, melliza1, melliza2), saved_clsfr)
+            saved_clsfr.close()
+
+
+
+
+
+
+#parser = argparse.ArgumentParser()
+#group = parser.add_mutually_exclusive_group()
+#group.add_argument("-v", "--verbose", action="store_true")
+#group.add_argument("-q", "--quiet", action="store_true")
+#parser.add_argument("x", type=int, help="the base")
+#parser.add_argument("y", type=int, help="the exponent")
+#args = parser.parse_args()
+#answer = args.x**args.y
+
+#if args.quiet:
+ #   print answer
+#elif args.verbose:
+ #   print "{} to the power {} equals {}".format(args.x, args.y, answer)
+#else:
+#    print "{}^{} == {}".format(args.x, args.y, answer)
 
         
 
