59d126c17a1dfd68fe360771f0751d4fe385a167
elizabethgarza <43279348+elizabethgarza@users.noreply.github.com>
2020-07-20 21:54:23 -0400

    Update diacriticize.py

--- src/diacriticize.py
+++ src/diacriticize.py
@@ -86,33 +86,117 @@ class Diacriticize:
         return features
 
 
-    def predict_token(self, tokenized_sentence, index, decoded_lowercased_token):
+    def predict_token(self, tokenized_sentence, index, unidec_melliza):
         
         self.tokenized_sentence = tokenized_sentence 
         self.index = index
-        self.decoded_lowercased_token = decoded_lowercased_token
+        self.unidec_melliza = unidec_melliza
         
         sentence_features = Diacriticize.extract_features(self.tokenized_sentence, self.index)
-        predicted_token = self.melliza_and_clsfr_dict[self.decoded_lowercased_token].classify(sentence_features)
+        predicted_token = self.melliza_and_clsfr_dict[self.unidec_melliza].classify(sentence_features)
         
         return predicted_token
-          
+
     
     def predict_sentence(self, tokenized_sentence): 
         
         self.tokenized_sentence = tokenized_sentence 
 
-        predicted_tokens = []
+        predicted_toks = []
         for (index,token) in enumerate(self.tokenized_sentence): 
-            decoded_lowercased_token = unidecode.unidecode(token.casefold())
-            if decoded_lowercased_token in self.melliza_and_clsfr_dict:
-                predicted_tokens.append(self.predict_token(self.tokenized_sentence, index, decoded_lowercased_token))
-            elif decoded_lowercased_token in self.invariantly_diacriticized_tokens_dict: 
-                predicted_tokens.append(self.invariantly_diacriticized_tokens_dict[decoded_lowercased_token])
+            unidec_melliza = unidecode.unidecode(token.casefold())
+            if unidec_melliza in self.melliza_and_clsfr_dict:
+                predicted_toks.append(self.predict_token(self.tokenized_sentence, index, unidec_melliza))
+            elif unidec_melliza in self.invariantly_diacriticized_tokens_dict: 
+                predicted_toks.append(self.invariantly_diacriticized_tokens_dict[unidec_melliza])
+            elif 'ion' in unidec_melliza[-3:]:
+                predicted_toks.append(unidec_melliza.replace('ion', 'ión'))
+            elif 'patia' in unidec_melliza[-5:]:
+                predicted_toks.append(unidec_melliza.replace('patia', 'patía'))
+            elif 'patias' in unidec_melliza[-6:]:
+                predicted_toks.append(unidec_melliza.replace('patias', 'patías'))  
+            elif 'grafía' in unidec_melliza[-6:]: 
+                predicted_toks.append(unidec_melliza.replace('grafia', 'grafía'))
+            elif 'grafías' in unidec_melliza[-7:]: 
+                predicted_toks.append(unidec_melliza.replace('grafias', 'grafías'))
+            elif 'latria' in unidec_melliza[-5:]:
+                predicted_toks.append(unidec_melliza.replace('latria', 'latría'))
+            elif 'eria' in unidec_melliza[-4:]:
+                predicted_toks.append(unidec_melliza.replace('eria', 'ería'))
+            elif 'erias' in unidec_melliza[-5:]:
+                predicted_toks.append(unidec_melliza.replace('erias', 'erías'))
+            elif 'latrias' in unidec_melliza[-6:]:
+                predicted_toks.append(unidec_melliza.replace('latrias', 'latrías'))
+            elif 'tomia' in unidec_melliza[-5:]:
+                predicted_toks.append(unidec_melliza.replace('tomia', 'tomía'))
+            elif 'tomias' in unidec_melliza[-6:]:
+                predicted_toks.append(unidec_melliza.replace('tomias', 'tomías'))
+            elif 'logia' in unidec_melliza[-5:]:
+                predicted_toks.append(unidec_melliza.replace('logia', 'logía'))
+            elif 'logias' in unidec_melliza[-6:]:
+                predicted_toks.append(unidec_melliza.replace('logias', 'logías'))
+            elif 'abamos' in unidec_melliza[-6:]:
+                predicted_toks.append(unidec_melliza.replace('abamos', 'ábamos'))
+            elif 'america' in unidec_melliza[-7:]:
+                predicted_toks.append(unidec_melliza.replace('america', 'américa'))
+            elif 'americas' in unidec_melliza[-8:]:
+                predicted_toks.append(unidec_melliza.replace('americas', 'américas'))    
+            elif 'logico' in unidec_melliza[-6:]:
+                predicted_toks.append(unidec_melliza.replace('logico', 'lógico'))
+            elif 'fobico' in unidec_melliza[-6:]:
+                predicted_toks.append(unidec_melliza.replace('fobico', 'fóbico'))
+            elif 'fobicos' in unidec_melliza[-6:]:
+                predicted_toks.append(unidec_melliza.replace('fobicos', 'fóbicos'))
+            elif 'logicos' in unidec_melliza[-8:]:
+                predicted_toks.append(unidec_melliza.replace('logicos', 'lógicos'))
+            elif 'zon' in unidec_melliza[-3:]:
+                predicted_toks.append(unidec_melliza.replace('zon', 'zón'))
+            elif 'zones' in unidec_melliza[-5:]:
+                predicted_toks.append(unidec_melliza.replace('zones', 'zónes'))
+            elif 'scopico' in unidec_melliza[-7:]:
+                predicted_toks.append(unidec_melliza.replace('scopico', 'scópico'))
+            elif 'scopicos' in unidec_melliza[-7:]:
+                predicted_toks.append(unidec_melliza.replace('scopicos', 'scópicos'))
+            elif 'onimo' in unidec_melliza[-5:]:
+                predicted_toks.append(unidec_melliza.replace('onimo', 'ónimo'))
+            elif 'onimos' in unidec_melliza[-6:]:
+                predicted_toks.append(unidec_melliza.replace('onimos', 'ónimos'))
+            elif 'onicos' in unidec_melliza[-6:]:
+                predicted_toks.append(unidec_melliza.replace('onicos', 'ónicos'))
+            elif 'onicas' in unidec_melliza[-6:]:
+                predicted_toks.append(unidec_melliza.replace('onicas', 'ónicas'))
+            elif 'onica' in unidec_melliza[-5:]:
+                predicted_toks.append(unidec_melliza.replace('onica', 'ónica'))
+            elif 'onico' in unidec_melliza[-5:]:
+                predicted_toks.append(unidec_melliza.replace('onico', 'ónico'))
+            elif 'ectomias' in unidec_melliza[-8:]: 
+                predicted_toks.append(unidec_melliza.replace('ectomias', 'ectomías'))
+            elif 'ectomia' in unidec_melliza[-7:]: 
+                predicted_toks.append(unidec_melliza.replace('ectomia', 'ectomía'))
+            elif 'cigotico' in unidec_melliza[-8:]: 
+                predicted_toks.append(unidec_melliza.replace('cigotico', 'cigótigo'))
+            elif 'cigoticos' in unidec_melliza[-9:]: 
+                predicted_toks.append(unidec_melliza.replace('cigoticos', 'cigótigos'))     
+            elif 'centrico' in unidec_melliza[-9:]: 
+                predicted_toks.append(unidec_melliza.replace('centrico', 'céntrico'))
+            elif 'centricos' in unidec_melliza[-9:]: 
+                predicted_toks.append(unidec_melliza.replace('centricos', 'céntricos'))  
+            elif 'aceo' in unidec_melliza[-4:]: 
+                predicted_toks.append(unidec_melliza.replace('aceo', 'áceo'))
+            elif 'aceos' in unidec_melliza[-4:]: 
+                predicted_toks.append(unidec_melliza.replace('aceos', 'áceos'))
+            elif 'orico' in unidec_melliza[-5:]: 
+                predicted_toks.append(unidec_melliza.replace('orico', 'órico'))
+            elif 'oricos' in unidec_melliza[-5:]: 
+                predicted_toks.append(unidec_melliza.replace('oricos', 'óricos'))
+            elif 'iendose' in unidec_melliza[-7:]: 
+                predicted_toks.append(unidec_melliza.replace('iendose', 'iéndose'))
+            elif 'ificamente' in unidec_melliza[-7:]: 
+                predicted_toks.append(unidec_melliza.replace('ificamente', 'íficamente'))           
             else: 
-                predicted_tokens.append(decoded_lowercased_token)
+                predicted_toks.append(unidec_melliza)
             
-        return predicted_tokens
+        return predicted_toks
 
 if __name__ =='__main__': 
     parser = argparse.ArgumentParser(description='Diacriticizes a sequence of unidecoded Spanish tokens.')
@@ -141,3 +225,4 @@ if __name__ =='__main__':
             continue 
         i+=1
     print(' '.join(predicted_tokens))
+
